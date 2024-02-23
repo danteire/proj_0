bb1067a8c226c87b359cfc0ebcb67bb00a377192
Dante <pioter814@gmail.com>
2024-02-23 23:53:22 +0100

    first commit

--- _silnik_ze_smieci/src/main.c
+++ _silnik_ze_smieci/src/main.c
@@ -5,7 +5,6 @@
 #include <SDL2/SDL.h>
 
 
-
 int main(int argc, char **argv) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,3);
